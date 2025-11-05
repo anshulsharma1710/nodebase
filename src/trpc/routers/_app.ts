1b3f88f49f39e8252cbf23f694aa615dd6b4593d
anshulsharma1710 <workwithfloody@gmail.com>
2025-11-05 13:24:21 +0530

    Payment Setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,9 @@
 import { inngest } from '@/inngest/client';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { createTRPCRouter, premiumProcedure, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
-import { TRPCError } from '@trpc/server';
 
 export const appRouter = createTRPCRouter({
-  testAi: baseProcedure.mutation(async () => {
-    // throw new TRPCError({ code: "BAD_REQUEST", message: "Something went wrong" });
+  testAi: premiumProcedure.mutation(async () => {
   
     await inngest.send({
       name: "execute/ai",
