bbd8df0450448d7021cc1dc0558d0209b23c34ee
anshulsharma1710 <workwithfloody@gmail.com>
2026-01-12 19:23:47 +0530

    AI Nodes Started

---
+++ src/features/executions/components/gemini/node.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useReactFlow, type Node, type NodeProps } from "@xyflow/react";
+import { memo, useState } from "react";
+import { BaseExecutionNode } from "../base-execution-node";
+import { AVAILABLE_MODELS, GeminiDialog, GeminiFormValues } from "./dialog";
+import { useNodeStatus } from "../../hooks/use-node-status";
+import { fetchHttpRequestRealtimeToken } from "./actions";
+import { HTTP_REQUEST_CHANNEL_NAME } from "@/inngest/channels/http-request";
+
+type GeminiNodeData = {
+    variableName?: string;
+    model?: "gemini-1.5-flash" | "gemini-1.5-flash-8b" | "gemini-1.5-pro" | "gemini-1.0-pro" | "gemini-pro";
+    systemPrompt?: string;
+    userPrompt?: string;
+};
+
+type GeminiNodeType = Node<GeminiNodeData>;
+
+export const GeminiNode = memo((props: NodeProps<GeminiNodeType>) => {
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const { setNodes } = useReactFlow();
+
+    const nodeStatus = useNodeStatus({
+        nodeId: props.id,
+        channel: HTTP_REQUEST_CHANNEL_NAME,
+        topic: "status",
+        refreshToken: fetchHttpRequestRealtimeToken,
+    });
+
+    const handleOpenSettings = () => setDialogOpen(true);
+
+    const handleSubmit = (values: GeminiFormValues) => {
+        setNodes((nodes) => nodes.map((node) => {
+            if (node.id === props.id) {
+                return {
+                    ...node,
+                    data: {
+                        ...node.data,
+                        ...values,
+                    }
+                }
+            }
+            return node;
+        }))
+    };
+
+    const nodeData = props.data;
+    const description = nodeData?.userPrompt
+        ? `${nodeData.model || AVAILABLE_MODELS[0]}: ${nodeData.userPrompt.slice(0, 50)}...`
+        : "Not configured";
+    
+    return (
+        <>
+            <GeminiDialog 
+                open={dialogOpen}
+                onOpenChange={setDialogOpen}
+                onSubmit={handleSubmit}
+                defaultValues={nodeData}
+            />
+            <BaseExecutionNode 
+                {...props}
+                id={props.id}
+                icon="/logos/gemini.svg"
+                name="Gemini"
+                status={nodeStatus}
+                description={description}
+                onSettings={handleOpenSettings}
+                onDoubleClick={handleOpenSettings}
+            />
+        </>
+    );
+});
+
+GeminiNode.displayName = "GeminiNode";
